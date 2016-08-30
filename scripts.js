4b7eb12b4f5225249a6e9fc2864900205360550c
Alex Pilewski <pilewski.alex@gmail.com>
2016-08-30 15:19:05 -0600

    populates bookmark field with title and url

--- scripts.js
+++ scripts.js
@@ -1,6 +1,19 @@
+$(document).ready(function() {
 // PHASE ONE
 // User can input a title and URL into appropriate fields and when submit button is clicked, bookmark populates appropriate section
+var titleField;
+var urlField;
 
+$('.submit').click( function() {
+  var titleField = $('.title').val();
+  var urlField = $('.url').val();
+  createBookmark(titleField, urlField);
+});
+
+function createBookmark(x, y) {
+  $('.titleResult').text(x);
+  $('.urlResult').text(y);
+}
 // User clicks on "Mark as Read"
 // A class of '.read' is added to bookmark
 // If bookmark has class of '.read' class should be removed
@@ -21,3 +34,4 @@
 //PHASE FOUR
 //Add a "clear read bookmarks" button which clears bookmarks
 //User should not allow invalid 'URL' inputs
+});
